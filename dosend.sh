2e3523e2b0b725dc6c38fc3b691bd63840d80519
beaukode <5773637+beaukode@users.noreply.github.com>
2020-02-09 09:38:32 +0100

    Revert "Adding debug log"
    This reverts commit f97b602a4de742f36805c3bbaca9c900e25742d7.

--- dosend.sh
+++ dosend.sh
@@ -73,9 +73,6 @@ dosend_swift () {
             EXTRA_ARGS="$EXTRA_ARGS -H X-Delete-After:$OS_DELETE_AFTER"
         fi
 
-        echo swift -A "$OS_AUTH_URL" --os-username "$OS_USERNAME" --os-password "$OS_PASSWORD" --auth-version $OS_AUTH_VERSION \
-            --os-project-name "$OS_PROJECT_NAME" --os-region-name "$OS_REGION_NAME" upload $EXTRA_ARGS --object-name "$OBJNAME" "$OS_CONTAINER" $D
-
         swift -A "$OS_AUTH_URL" --os-username "$OS_USERNAME" --os-password "$OS_PASSWORD" --auth-version $OS_AUTH_VERSION \
             --os-project-name "$OS_PROJECT_NAME" --os-region-name "$OS_REGION_NAME" upload $EXTRA_ARGS --object-name "$OBJNAME" "$OS_CONTAINER" $D
     done
